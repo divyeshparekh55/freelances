7d8c5179ae41dc20b429af4db7af92105054c960
Divyesh Parekh <parekhdivyesh555@gmail.com>
2023-03-19 20:47:22 +0530

    update databasze

--- database/textile.sql
+++ database/textile.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 11, 2023 at 07:52 AM
+-- Generation Time: Mar 19, 2023 at 04:10 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -41,11 +41,55 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `admin_id`, `ctgy_name`, `sub_ctgy_type`, `user_id`) VALUES
-(1, 45, 'Divyesh', '1', '29'),
-(2, 45, 'Motor', '1', '29'),
-(5, 45, 'sas', '3', '29'),
-(6, 45, 'ssss', '1', '29'),
-(7, 45, 'sasassss', '4', '29');
+(1, 45, '300x600MM', '1', '29'),
+(2, 45, '600x600MM', '2', '29'),
+(3, 45, '600x1200MM', '1', '29'),
+(4, 45, '800x800MM', '1', '29'),
+(5, 45, '1200X1200MM', '1', '29'),
+(6, 45, 'dd', '1', '29'),
+(7, 45, '6X6MM', '1', '29'),
+(8, 45, '8X8MM', '1', '29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `postfix_images`
+--
+
+CREATE TABLE `postfix_images` (
+  `id` int(11) NOT NULL,
+  `img_nme` varchar(255) NOT NULL,
+  `img_location` varchar(255) NOT NULL,
+  `user_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `postfix_images`
+--
+
+INSERT INTO `postfix_images` (`id`, `img_nme`, `img_location`, `user_id`) VALUES
+(2, 'IMG-64106b6593e6e4.02149264.jpg', 'upload_images/IMG-64106b6593e6e4.02149264.jpg', '29'),
+(3, 'IMG-64106b733aed23.43312743.png', 'upload_images/IMG-64106b733aed23.43312743.png', '29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prefix_images`
+--
+
+CREATE TABLE `prefix_images` (
+  `id` int(11) NOT NULL,
+  `img_nme` varchar(255) NOT NULL,
+  `img_location` varchar(255) NOT NULL,
+  `user_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `prefix_images`
+--
+
+INSERT INTO `prefix_images` (`id`, `img_nme`, `img_location`, `user_id`) VALUES
+(5, 'IMG-64106b57cd7594.59245670.jpg', 'upload_images/IMG-64106b57cd7594.59245670.jpg', '29');
 
 -- --------------------------------------------------------
 
@@ -66,9 +110,9 @@ CREATE TABLE `register` (
 --
 
 INSERT INTO `register` (`id`, `fname`, `password`, `user_type`, `is_loggedin`) VALUES
-(29, 'ks', '123', 'sub_admin', 0),
+(29, 'ks', '123', 'sub_admin', 1),
 (38, 'vatsal', 'vatsal', 'sub_admin', 0),
-(45, 'ar', '123', 'admin', 1),
+(45, 'ar', '123', 'admin', 0),
 (53, 'divyesh', '12345', 'sub_admin', 0),
 (54, 'divysh', '123', 'sub_admin', 0),
 (56, 'divyessh', '1465', 'admin', 0);
@@ -90,10 +134,10 @@ CREATE TABLE `sub_ctgy` (
 --
 
 INSERT INTO `sub_ctgy` (`id`, `admin_id`, `ctgy_name`) VALUES
-(1, 45, ' water'),
-(2, 45, ' motor'),
-(3, 45, ' sasa'),
-(4, 45, ' sasaas');
+(1, 45, ' MatteGlossy'),
+(2, 45, ' Satin Matt'),
+(3, 45, ' Sugar Surface'),
+(4, 45, ' High Glossy');
 
 -- --------------------------------------------------------
 
@@ -103,7 +147,7 @@ INSERT INTO `sub_ctgy` (`id`, `admin_id`, `ctgy_name`) VALUES
 
 CREATE TABLE `upload_image` (
   `id` int(11) NOT NULL,
-  `admin_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   `img_nme` varchar(255) NOT NULL,
   `img_path` varchar(255) NOT NULL
@@ -113,12 +157,8 @@ CREATE TABLE `upload_image` (
 -- Dumping data for table `upload_image`
 --
 
-INSERT INTO `upload_image` (`id`, `admin_id`, `category_id`, `img_nme`, `img_path`) VALUES
-(1, 29, 4, 'IMG-63e397469215d7.83465545.jpg', 'upload_images/IMG-63e397469215d7.83465545.jpg'),
-(2, 29, 1, 'IMG-63f7611a875673.88449882.jpg', 'upload_images/IMG-63f7611a875673.88449882.jpg'),
-(3, 38, 1, 'IMG-6400ab6f306667.96811508.jpg', 'upload_images/IMG-6400ab6f306667.96811508.jpg'),
-(4, 29, 1, 'IMG-6401ecaa7b4840.24457111.jpg', 'upload_images/IMG-6401ecaa7b4840.24457111.jpg'),
-(5, 29, 1, 'IMG-6401ecbc7b9ec8.61683651.jpg', 'upload_images/IMG-6401ecbc7b9ec8.61683651.jpg');
+INSERT INTO `upload_image` (`id`, `user_id`, `category_id`, `img_nme`, `img_path`) VALUES
+(4, 29, 1, 'IMG-6401ecaa7b4840.24457111.jpg', 'upload_images/IMG-6401ecaa7b4840.24457111.jpg');
 
 --
 -- Indexes for dumped tables
@@ -128,6 +168,18 @@ INSERT INTO `upload_image` (`id`, `admin_id`, `category_id`, `img_nme`, `img_pat
 -- Indexes for table `category`
 --
 ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `postfix_images`
+--
+ALTER TABLE `postfix_images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `prefix_images`
+--
+ALTER TABLE `prefix_images`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -156,7 +208,19 @@ ALTER TABLE `upload_image`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `postfix_images`
+--
+ALTER TABLE `postfix_images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `prefix_images`
+--
+ALTER TABLE `prefix_images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `register`
